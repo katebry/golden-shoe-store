d8922a9e7b19fb103d8b2914fcd7319070e8bfc6
Kate Bryan <katebryan31@gmail.com>
2021-04-10 14:23:47 +0100

    feat: brought in dotenv config to server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
