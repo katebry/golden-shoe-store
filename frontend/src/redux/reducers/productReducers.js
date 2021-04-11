4750d59cc56404c76c26489cdc9e53b40e93ed09
Kate Bryan <katebryan31@gmail.com>
2021-04-11 18:39:15 +0100

    feat: added productActions

--- frontend/src/redux/reducers/productReducers.js
+++ frontend/src/redux/reducers/productReducers.js
@@ -22,7 +22,6 @@ export const getProductsReducer = (state = { products: [] }, action) => {
   }
 };
 
-
 export const getProductDetailsReducer = (state = { product: {} }, action) => {
   switch (action.type) {
     case actionTypes.GET_PRODUCT_DETAILS_REQUEST:
