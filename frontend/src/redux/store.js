1b64a38e454d70e7ffd544e5b412f75c23dc8b2c
Kate Bryan <katebryan31@gmail.com>
2021-04-11 17:06:03 +0100

    fix: added productReducers to cstore

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { cartReducer } from "./reducers/cartReducer";
+import {
+  getProductsReducer,
+  getProductDetailsReducer,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
-  // this is where your different reducers go
   cart: cartReducer,
+  getProducts: getProductsReducer,
+  getProductDetails: getProductDetailsReducer,
 });
 
 // the middleware is used to make asynchronous requests in your actions
