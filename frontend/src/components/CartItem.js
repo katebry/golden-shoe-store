7a664ce573e8dd268e76b9d8130cb8ce92a629d0
Kate Bryan <katebryan31@gmail.com>
2021-04-12 09:07:28 +0100

    feat: added dynamic addToCart functionality

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,22 +1,22 @@
-import "./CartItem.css"
-import {Link} from "react-router-dom"
+import "./CartItem.css";
+import { Link } from "react-router-dom";
 
-export const CartItem = () => {
-    return (
-        <div className="cartitem">
-            <div className="cartitem__image">
-                <img src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" alt="" />
-            </div>
-            <Link to={`/product/${1}`} className="cartitem__name">
-                <p>Product Name</p>
-            </Link>
-            <p className="cartitem__price">£89.90</p>
-            <select className="cartitem__select">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-            <button className="cartitem__deleteBtn">Delete</button>
-        </div>
-    )
-}
+export const CartItem = ({ item }) => {
+  return (
+    <div className="cartitem">
+      <div className="cartitem__image">
+        <img src={item.imageUrl} alt={item.name} />
+      </div>
+      <Link to={`/product/${item.product}`} className="cartitem__name">
+        <p>{item.name}</p>
+      </Link>
+      <p className="cartitem__price">£{item.price}</p>
+      <select className="cartitem__select" value={item.quantity} onChange={() => console.log('yes')}>
+        {[...Array(item.countInStock).keys()].map(x => (
+            <option key={x+1} value={x+1}>{x+1}</option>
+        ))}
+      </select>
+      <button className="cartitem__deleteBtn">Delete</button>
+    </div>
+  );
+};
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,19 +1,34 @@
 import "./Cart.css";
 import { CartItem } from "../components/CartItem";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const CartPage = () => {
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <div className="cartpage">
       <div className="cartpage__left">
         <h2>Shopping Cart</h2>
-        <CartItem />
+        {cartItems.length === 0 ? (
+          <div>
+            Your cart is empty <Link to="/">Back to Shop</Link>
+          </div>
+        ) : (
+          cartItems.map((item) => <CartItem item={item}/>)
+        )}
       </div>
       <div className="cartpage__right">
-          <div className="cartpage__info">
-              <p>Subtotal: 0 items</p>
-              <p>Price: £34.99</p>
-          </div>
-          <div><button className="cartpage__btn">Proceed to Checkout</button></div>
+        <div className="cartpage__info">
+          <p>Subtotal: 0 items</p>
+          <p>Price: £34.99</p>
+        </div>
+        <div>
+          <button className="cartpage__btn">Proceed to Checkout</button>
+        </div>
       </div>
     </div>
   );
