49d4cc9eae5e7f867dc6486230e8160b0b780e68
Kate Bryan <katebryan31@gmail.com>
2021-04-13 10:55:18 +0100

    chore: added sale image to Homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,23 +2,34 @@ import "./Home.css";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Product } from "../components/index";
-import { getAllProducts} from "../redux/actions/productActions";
+import { getAllProducts } from "../redux/actions/productActions";
+import sale from "../assets/sale.png";
 
 export const HomePage = () => {
   const dispatch = useDispatch();
 
   const getProducts = useSelector((state) => state.getProducts);
-  
+
   const { products, loading, error } = getProducts;
 
   useEffect(() => {
     dispatch(getAllProducts());
-    console.log('calling useEffect hook')
+    console.log("calling useEffect hook");
   }, [dispatch]);
 
   return (
     <div className="homepage">
-      <h2 className="homepage__title">Products</h2>
+      <div className="topContent">
+        <div className="searchSection">
+          Category Filter
+        </div>
+        <div className="saleImage">
+          <img
+            src={sale}
+            alt="20% off kids shoes, enter code 'KID20'. Spend £60 and get £10 off your purchase (excluding kids shoes)"
+          />
+        </div>
+      </div>
       <div className="homepage__products">
         {loading ? (
           <h2>LOADING...</h2>
