28860e98199c9770446121dff73ce707737431c1
Kate Bryan <katebryan31@gmail.com>
2021-04-15 13:11:38 +0100

    feat: saved authToken to localStorage

--- frontend/src/redux/reducers/authReducer.js
+++ frontend/src/redux/reducers/authReducer.js
@@ -3,9 +3,11 @@ import { AUTH, LOGOUT } from "../constants/authConstants";
 export const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
-      console.log(action?.data);
-      return state;
-
+      localStorage.setItem(
+        "godenShoesProfile",
+        JSON.stringify({ ...action?.data })
+      );
+      return { ...state, authData: action.data };
     default:
       return state;
   }
