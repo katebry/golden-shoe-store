a74e4113c3b0bc10b532f4b53e2542d8edaf8b69
Kate Bryan <katebryan31@gmail.com>
2021-04-15 11:23:27 +0100

    feat: added form fields and conditional logic

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,9 +2,18 @@ import { useState } from "react";
 import { Input } from "../components/index";
 
 export const LoginPage = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const isNewUser = true;
+  const initialState = { firstName: "", lastName: "", email: "", password: "" };
+
+  const [form, setForm] = useState(initialState);
+  const [isNewUser, setIsNewUser] = useState(false)
+
+const switchMode = () => {
+    setIsNewUser(!isNewUser)
+}
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = () => {
     console.log("yes");
@@ -12,7 +21,7 @@ export const LoginPage = () => {
 
   return (
     <>
-      {isNewUser ? <h2>Login</h2> : <h2>Sign up</h2>}
+      {isNewUser ? <h2>Sign up</h2> : <h2>Login</h2>}
       <form onSubmit={handleSubmit}>
         {isNewUser && (
           <div>
@@ -20,16 +29,36 @@ export const LoginPage = () => {
               type="text"
               label="First name"
               placeholder="First name"
-              onChange={(e) => setFirstName(e.target.value)}
+              onChange={handleChange}
             />
             <Input
               type="text"
               label="Last name"
               placeholder="Last name"
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={handleChange}
             />
           </div>
         )}
+        <Input
+          type="email"
+          label="Email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          label="Password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>
+          {isNewUser ? "Sign up" : "Sign in"}
+        </button>
+        <button onClick={switchMode}>
+          {isNewUser
+            ? "Already have an account? Sign in"
+            : "Don't have an account? Sign Up"}
+        </button>
       </form>
     </>
   );
