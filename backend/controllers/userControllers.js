f8be9e31afb5e748b47a631b6e27f137a9ed5327
Kate Bryan <katebryan31@gmail.com>
2021-04-15 16:25:37 +0100

    feat: added User Schema and postExistingUser controller

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,8 +1,34 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const postNewUser = async (req, res) => {};
 
-const postExistingUser = async (req, res) => {};
+const postExistingUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "User does not exist." });
+
+    const correctPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!correctPassword)
+      return res.status(400).json({ message: "Invalid login credentials." });
+
+    // the second argument would normally be a secret key
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong." });
+  }
+};
 
 module.exports = { postNewUser, postExistingUser };
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  postCode: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+});
 
 const User = mongoose.model("user", userSchema);
 
